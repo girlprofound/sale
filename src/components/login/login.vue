be1d05ffaf8cba42e5c30d529ca689eb860445dc
girlprofound <l18178070171@163.com>
2020-10-30 22:48:20 +0800

    重新提交代码

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,12 +1,31 @@
 <template>
-  <div>登录</div>
+  <div>
+    <el-form
+      :label-position="top"
+      label-width="80px"
+      :model="formdata"
+    >
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
-
+    data(){
+        return{
+            formdata: {
+                username: '',
+                password: ''
+            }
+        }
+    }
+};
 </script>
 <style scoped>
 </style>
